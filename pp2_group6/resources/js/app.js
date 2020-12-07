3b424904abf108a0c15a1dfdb04ce0019cb7d942
adil_el_gham <adil.el.ghamarti@student.ehb.be>
2020-12-07 20:22:53 +0100

    <feat>: <add page manage request>

--- pp2_group6/resources/js/app.js
+++ pp2_group6/resources/js/app.js
@@ -1,11 +1,16 @@
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 require('./bootstrap');
 
-
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 // Declaration
 window.Vue = require('vue');
 
 // New component
-Vue.component('face', require('./components/Face.vue').default);
+Vue.component('manage', require('./components/manage.vue').default);
 
 // Instantiation
 window.onload = function(e) {
--- pp2_group6/resources/views/welcome.blade.php
+++ pp2_group6/resources/views/welcome.blade.php
@@ -3,15 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Laravel & Vue</title>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
 
         <script src="<?php echo asset('/js/app.js')?>"></script>
+        
     </head>
     <body>
         <div id="app" style="text-align:center;margin-top:2%;margin-bottom:2%;">
-            
         </div>
     </body>
 </html>
