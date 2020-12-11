8057118a664f51506fe1ebb13e7e98aaac22ffa5
ElyasHamich <elyas.hamich@student.ehb.be>
2020-12-11 10:09:10 +0100

    feat: layout adjust

--- pp2_group6/resources/views/layout.blade.php
+++ pp2_group6/resources/views/layout.blade.php
@@ -9,6 +9,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link href="{{ asset('css/layout.css') }}" rel="stylesheet">
 
         <script src="{{ asset('js/app.js') }}"></script>
     </head>
