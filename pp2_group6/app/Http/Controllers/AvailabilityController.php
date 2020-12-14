104245dfee6bbbe8a55ed6786278792451acfa23
yassblass <yassine.tanouti@student.ehb.be>
2020-12-14 15:23:11 +0100

    feat: Added getAvailabilities function in AvailabilityController.

--- pp2_group6/app/Http/Controllers/AvailabilityController.php
+++ pp2_group6/app/Http/Controllers/AvailabilityController.php
@@ -8,7 +8,15 @@ class AvailabilityController extends Controller
 {
     public function getAvailabilities () {
 
+        //Get all secretary availabilities where status = 'free' and store them.
         
+        if($availabilities = Availability::where('status', 'free')){
 
+            return respone(true);
+        }
+        else{
+            return respone(false);
+        }
+        
     }
 }
