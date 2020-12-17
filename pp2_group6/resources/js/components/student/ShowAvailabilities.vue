762fe9cec1a2288caa6fee7bdc824b582c3cb0bd
SouheibTouri <45950693+SouheibTouri@users.noreply.github.com>
2020-12-17 17:42:40 +0100

    feat: error message when no date selected + fixed date logic

--- pp2_group6/resources/js/components/student/ShowAvailabilities.vue
+++ pp2_group6/resources/js/components/student/ShowAvailabilities.vue
@@ -20,22 +20,28 @@
       </table> -->
 
       <b-form-group label="Choose a date">
-        <b-form-input type="date" placeholder="Date" v-model="request.date">
-        </b-form-input>
+            <b-form-datepicker 
+              v-model="selectedDate" 
+              :min="min" 
+              :date-disabled-fn="dateDisabled"
+              :state="state"
+              reset-button
+              close-button>
+            </b-form-datepicker>
       </b-form-group>
+
+      {{ request.date }}
 
 <b-form-group label="Choose between available secretary">
         <b-form-radio-group
           v-model="request.user_id"
           buttons
-          button-variant="danger"
-        >
+          button-variant="danger">
           <template v-for="user in users">
             <b-form-radio
               :value="user.user_id"
               :key="user.user_id"
-              v-model="selectedSecretary"
-            >
+              v-model="selectedSecretary">
               {{ user.firstName + " " + user.lastName }}
             </b-form-radio>
           </template>
@@ -71,9 +77,16 @@
 <script>
 import { mapGetters } from "vuex";
 export default {
-  
-    
   data() {
+    const now = new Date()
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+
+    const minDate = new Date(today)
+    minDate.setYear(minDate.getFullYear())
+    minDate.setMonth(minDate.getMonth())
+    minDate.setDate(minDate.getDate() + 2)
+
+
     return {
       request: {
         user_id: "",
@@ -83,17 +96,30 @@ export default {
       availabilities: {},
       selectedSecretary : '',
       chosenTime : '',
-
+      min: minDate,
+      state: false,
+      selectedDate: ''
     };
   },
   created() {
 
+  },
+  methods: {
+    dateDisabled(ymd, date) {
+        const weekday = date.getDay()
+
+        return weekday === 0 || weekday === 6
+    }
   },
   watch: {
       // time : function(newTime){
 
       //     this.chosenTime = nexTime;
       // },
+      selectedDate: function() {
+          this.state = true;
+          this.request.date = this.selectedDate;
+      },
       selectedSecretary: function (newSecretary) {
       this.selectedSecretary = newSecretary;
       //this.currentComponent = "show-Availabilities";
@@ -105,7 +131,8 @@ export default {
             secretaryId : secretaryId,
             date : this.request.date,
         } )
-        .then((response) => (this.availabilities = response.data))
+        .then((response) => (
+          this.availabilities = response.data))
         .catch((error) => console.log(error));
 
         
@@ -119,7 +146,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(["users"]),
+    ...mapGetters(["users"])
   },
 };
 </script>
