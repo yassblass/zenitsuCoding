81bbbb608cfd515293ad0a42aeb3d919bfa61f4b
SouheibTouri <45950693+SouheibTouri@users.noreply.github.com>
2020-12-19 01:53:01 +0100

    feat: added error messages for verification code and checkEmail(flagged, hasRight)

--- pp2_group6/resources/js/components/student/checkEmail.vue
+++ pp2_group6/resources/js/components/student/checkEmail.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="container">
+    <!-- First name error -->
     <b-alert v-model="showFirstName" variant="danger" dismissible>
       {{ errors.get("firstName") }}
     </b-alert>
+    <!-- Last name error -->
     <b-alert v-model="showLastName" variant="danger" dismissible>
       {{ errors.get("lastName") }}
     </b-alert>
+    <!-- Flagged error -->
+    <b-alert v-model="showFlagged" variant="danger" dismissible>
+      Your email is flagged, try again later!
+    </b-alert>
+    <!-- hasRight error -->
+    <b-alert v-model="showHasRight" variant="danger" dismissible>
+      Your email has no rights, try again later!
+    </b-alert>
+    <!-- Email does not exist in organization error -->
+    <b-alert v-model="showUnknown" variant="danger" dismissible>
+      Your email does not exist in our organization!
+    </b-alert>
+    <!-- General error -->
+    <b-alert v-model="showError" variant="danger" dismissible>
+      Oops! Something went wrong, try again.
+    </b-alert>
+
     <b-form-group label="Insert your first name">
       <b-form-input
         type="text"
@@ -54,6 +73,10 @@ export default {
       errors: new Errors(),
       showFirstName: false,
       showLastName: false,
+      showFlagged: false,
+      showHasRight: false,
+      showUnknown: false,
+      showError: false
     };
   },
   created() {},
@@ -68,25 +91,41 @@ export default {
           if (this.response === 1) {
             //If email exists
             console.log("Email exists!");
-
             //SEND TRUE to Parent.
             this.$emit("nameChecked", [1, this.student]);
           } else if (this.response === 2) {
             //If student is flagged
+            this.showFlagged = true;
             console.log("You have been flagged, try again later.");
           } else if (this.response === 3) {
             //If student has no rights
+            this.showHasRight = true;
             console.log("You have no rights, try again later.");
           } else if (this.response === 0) {
             //If email has not been found in DB
+            this.showUnknown = true;
             console.log("Email does not exist in organization!");
           } else {
+            this.showError = true;
             console.log("Something went wrong, try again!");
           }
         })
         .catch((err) => {
-          this.showFirstName = true;
-          this.showLastName = true;
+          
+     
+
+          if(err.response.data.errors['firstName'] && err.response.data.errors['lastName']){
+            this.showFirstName = true;
+              this.showLastName = true;
+          } else if (err.response.data.errors['lastName']){
+            this.showFirstName = false;
+            this.showLastName = true;
+          } else {
+              this.showFirstName = true;
+              this.showLastName = false;
+          }
+
+          // console.log(err.response.data.errors['firstName']);
 
           this.errors.record(err.response.data);
         });
