c3174df8c5aa379051b8484eea7bb676bf6bc0f0
SouheibTouri <45950693+SouheibTouri@users.noreply.github.com>
2020-12-20 14:34:36 +0100

    fix: small fixes + changed endMessage pages

--- pp2_group6/resources/js/components/student/ShowEndMessage.vue
+++ pp2_group6/resources/js/components/student/ShowEndMessage.vue
@@ -3,6 +3,8 @@
    
     
     <h3>An Appointment Request has been sent. Please check your mailbox.</h3>
+    <br>
+     <b-icon icon="check-circle" animation="throb" font-scale="4"></b-icon>
 </div>
 </template>
 <script>
--- pp2_group6/resources/views/student/appointments.blade.php
+++ pp2_group6/resources/views/student/appointments.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel Vue.js app</title>
+        <title>EhB | Appointment</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
